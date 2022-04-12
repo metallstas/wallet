672c604a1d467bc64bbeefb3fe1221afbc158bc9
Vladimir Hmylko <billizzard@mail.ru>
2022-04-12 13:35:54 +0300

    111

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,7 @@ import cls from './Main.module.css'
 export const Main = () => {
   return (
     <main>
-      <button alt='Add category' className={cls.button}>+</button>
+      <button className={cls.button}>+</button>
     </main>
   )
 }
