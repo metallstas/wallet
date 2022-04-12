f2c2734f9f798793fd58f2888f11afa351b0f8e6
Stas Khmylko <metallstas@mail.ru>
2022-04-12 13:42:55 +0300

    WALLET-3 Change background click button

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import cls from './Main.module.css'
+
+export const Main = () => {
+  const [isBlueButton, setIsBlueButton] = useState<boolean>(false)
+
+  const onClickChangeBackground = () => {
+    setIsBlueButton((prev) => !prev)
+  }
+  const styleBtn = {
+    background: isBlueButton ? 'blue' : '',
+    color: isBlueButton ? '#fff' : '',
+  }
+  
+  return (
+    <main>
+      <button
+        style={styleBtn}
+        onClick={onClickChangeBackground}
+        className={cls.button}
+      >
+        +
+      </button>
+    </main>
+  )
+}
