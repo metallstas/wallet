11ca1b0bf85d23c1deab248d25cf83096621a7f5
Vladimir Hmylko <billizzard@mail.ru>
2022-04-12 13:18:44 +0300

    Add alt description

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,7 @@ import cls from './Main.module.css'
 export const Main = () => {
   return (
     <main>
-      <button className={cls.button}>+</button>
+      <button alt='Add category' className={cls.button}>+</button>
     </main>
   )
 }
